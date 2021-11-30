0c94f4f3ed28757fdce64f487f70182ee7675d37
leilaaua <leila0777@icloud.com>
2021-11-30 15:41:18 +0400

    Initial Commit

---
+++ Simpsons/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Simpsons
+//
+//  Created by leila leila on 30/11/2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
